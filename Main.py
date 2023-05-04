d3fb1c3bf98527f5b391f7020bb4e535318b9802
Tristan678 <tristan.ganahl@gmail.com>
2023-05-04 16:59:21 +0200

    Added code
    I have added what I believe to be the complete code for the task. Feel free to make any changes/corrections if any present.

--- Main.py
+++ Main.py
@@ -6,3 +6,30 @@ Created on Thu May  4 15:53:17 2023
 @author: ganaht1
 """
 
+def read_from_file(filename):
+    try:
+        with open(filename, 'r') as file:
+            contents = file.read()
+            return contents
+    except FileNotFoundError:
+        print(f"Error: File '{filename}' not found.")
+        return None
+
+def write_to_file(filename, contents):
+    try:
+        with open(filename, 'w') as file:
+            file.write(contents)
+    except Exception as e:
+        print(f"Error: {e}")
+        return False
+    return True
+
+filename = 'example.txt'
+contents = read_from_file(filename)
+
+if contents is not None:
+    success = write_to_file(filename, contents)
+    if success:
+        print("Success: File written.")
+    else:
+        print("Error: Failed to write file.")
--- Tristan.py
+++ Tristan.py
@@ -8,14 +8,22 @@ Created on Thu May  4 15:39:50 2023
 
 
 
+
+
 import csv
 
 def read_csv_file(filename):
+
     with open(filename, 'r') as csvfile:
+       
         reader = csv.reader(csvfile)
+
         output = []
+
         for row in reader:
             if len(row) > 0:
                 output.append(row)
+
         return output
+
 

dfed76e9217da40009a657af7869bd34c8d961db
Tristan678 <tristan.ganahl@gmail.com>
2023-05-04 16:16:35 +0200

    Addition

---
+++ Main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May  4 15:53:17 2023
+
+@author: ganaht1
+"""
+
---
+++ Tristan.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May  4 15:39:50 2023
+
+@author: ganaht1
+"""
+
+
+import csv
+
+def read_csv_file(filename):
+    with open(filename, 'r') as csvfile:
+        reader = csv.reader(csvfile)
+        output = []
+        for row in reader:
+            if len(row) > 0:
+                output.append(row)
+        return output
+
